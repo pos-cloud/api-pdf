a4dc42981afa93ca1a05cfc6091a39d888ae5678
Franco Androetto <androettofranco@gmail.com>
2023-09-05 21:37:05 -0300

    feat: update models

--- app/models/article.ts
+++ app/models/article.ts
@@ -1,58 +1,79 @@
-export default interface ArticleI {
-    creationDate: string
-    type: string
-    order: number
-    containsVariants: boolean
-    containsStructure: boolean
-    code: string
-    codeProvider: string
-    codeSAT: string
-    description: string
-    url: string
-    posDescription: string
-    quantityPerMeasure: string
-    observation: string
-    notes: []
-    tags: []
-    basePrice: number
-    costPrice: number
-    markupPercentage: number
-    markupPrice: number
-    salePrice: number
+import Model from "./model"
 
-    locations: {
-      location: Location
-    }[]
-    children: {
-      article: ArticleI
-      quantity: number
-    }[]
-    pictures: {
-      wooId?: string
-      meliId?: string
-      picture: string
-    }[]
-    barcode: string
-    wooId: string
-    meliId: string
-    printIn: string
-    posKitchen: boolean
-    allowPurchase: boolean
-    allowSale: boolean
-    allowStock: boolean
-    allowSaleWithoutStock: boolean
-    allowMeasure: boolean
-    ecommerceEnabled: boolean
-    favourite: boolean
-    isWeigth: boolean
-    forShipping: boolean
+export default interface Article extends Model {
+  creationDate: string
+  //creationUser: User
+  type: string
+  order: number
+  containsVariants: boolean
+  containsStructure: boolean
+  code: string
+  codeProvider: string
+  codeSAT: string
+  description: string
+  url: string
+  posDescription: string
+  quantityPerMeasure: string
+  //unitOfMeasurement: UnitOfMeasurement
+  observation: string
+  notes: []
+  tags: []
+  basePrice: number
+  otherFields: {
+    //articleField: ArticleField
+    value: string
+    amount: number
+  }[]
+  //taxes: Taxes[]
+  costPrice: number
+  markupPercentage: number
+  markupPrice: number
+  salePrice: number
+  //currency: Currency
+  //make: Make
+  //category: Category
+  deposits: {
+   // deposit: Deposit
+    capacity: number
+  }[]
+  locations: {
+    location: Location
+  }[]
+  children: {
+    article: Article
+    quantity: number
+  }[]
+  pictures: {
+    wooId?: string
+    meliId?: string
     picture: string
-    providers: []
-    harticle: ArticleI
-    minStock: Number
-    maxStock: Number
-    pointOfOrder: Number
-    purchasePrice: Number
-    costPrice2: Number
-  }
-  
+  }[]
+  barcode: string
+  wooId: string
+  meliId: string
+  //meliAttrs: IMeliAttrs
+  printIn: string
+  posKitchen: boolean
+  allowPurchase: boolean
+  allowSale: boolean
+  allowStock: boolean
+  allowSaleWithoutStock: boolean
+  allowMeasure: boolean
+  ecommerceEnabled: boolean
+  favourite: boolean
+  isWeigth: boolean
+  forShipping: boolean
+  picture: string
+  providers: []
+  //provider: Company
+  //applications: Application[]
+  //classification: Classification
+  harticle: Article
+  //salesAccount: Account
+  //purchaseAccount: Account
+  minStock: Number
+  maxStock: Number
+  pointOfOrder: Number
+  purchasePrice: Number
+  costPrice2: Number
+}
