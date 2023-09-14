794aa52868b61c7f41c84349a6c93025938b73e5
jeremias vallejos <jeremias.vallejos78gmail.com>
2023-09-14 13:00:26 -0300

    fix: change nodemon

--- app/controllers/getPrintTransaction.ts
+++ app/controllers/getPrintTransaction.ts
@@ -75,12 +75,13 @@ export async function getPrintTransaction(
         doc.text(transaction.letter, 104.5, 14);
         doc.setFontSize(20);
         doc.text(config.companyPicture !== 'default.jpg' ? config.companyPicture : config.companyName , 15, 16);
-        doc.text(transactionType.name, 130, 16); // traer type service
+        doc.text(transactionType.name, 130, 16);
         doc.setFont("helvetica", "normal");
 
         // Labels Primer Cuadro
         doc.setFontSize(8);
         if (transactionType.codes && config.country === 'AR') {
+            
             for (let i = 0; i < transactionType.codes.length; i++) {
               if (
                 transactionType.codes[i].code &&
@@ -88,7 +89,7 @@ export async function getPrintTransaction(
               ) {
                 doc.setFontSize('8');
                 doc.text(
-                  'Coddd:' + padString(transactionType.codes[i].code.toString(), 2),
+                  'Cod:' + padString(transactionType.codes[i].code.toString(), 2),
                   101.4,
                   18,
                 );
@@ -96,28 +97,30 @@ export async function getPrintTransaction(
             }
           }
 
+        doc.setFont("helvetica", "bold");
         doc.setFontSize(9);
         doc.text(`Razón Social:  ${config.companyName}`, 9, 28);
         doc.text(`Domicilio Comercial:   ${config.companyAddress}`, 9, 35);
-        doc.text(`Condicion frente al IVA:   ${vatConditionsConfig?.description || ""}`, 9, 42);
+        doc.text(`Condición de IVA:   ${vatConditionsConfig?.description || ""}`, 9, 42);
 
         doc.text(`Punto de Venta: ${padString(transaction.origin, 4)}`, 120, 26);
         doc.text(`Comp. Nro: ${padString(transaction.number, 10)}`, 165, 26);
         doc.text(`Fecha de Emición: ${formatDate(transaction.startDate)} `, 120, 30);
 
-        doc.text(`CUIT: ${config.companyIdentificationValue}`, 120, 38);
+        doc.text(`C.U.I.T: ${config.companyIdentificationValue}`, 120, 38);
         doc.text(`Ingresos Brutos: ${config.companyGrossIncome}`, 120, 42);
-        doc.text(`Fecha de Inicio de Actividades: ${formatDate(config.companyStartOfActivity)}`, 120, 46);
+        doc.text(`Inicio de Actividades: ${formatDate(config.companyStartOfActivity)}`, 120, 46);
 
         // Labels Segundo Cuadro
         doc.setFontSize(8.9);
-        doc.text(`CUIT: ${company?.CUIT || ""}`, 9, 57);
-        doc.text(`Condicion frente al IVA: ${vatConditionsCompany?.description || "Consumidor Final"}`, 9, 62);
-        doc.text(`Condicion de venta:`, 9, 67);
+        doc.text(`C.U.I.T: ${company?.CUIT || ""}`, 9, 57);
+        doc.text(`Condición de IVA: ${vatConditionsCompany?.description || "Consumidor Final"}`, 9, 62);
+        doc.text(`Condición de venta:`, 9, 67);
         doc.text(`Apellido y Nombre / Razón Social: ${company?.name || ""}`, 100, 57);
         doc.text(`Domicilio Comercial:  ${company?.address || ""}`, 100, 62);
 
         doc.autoPrint();
+        doc.save('factura.pdf')
         const pdfBase64 = doc.output("datauristring");
         return res.status(200).send({ pdfBase64 });
     } catch (error) {
