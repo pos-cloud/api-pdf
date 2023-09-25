ef4098026efe8781216d565c35ed71f132dff9e8
Franco Androetto <androettofranco@gmail.com>
2023-09-25 16:13:46 -0300

    test

--- app/server.ts
+++ app/server.ts
@@ -1,18 +1,19 @@
-import * as express from 'express';
-import App from './app';
+import * as express from "express";
+import App from "./app";
 
-const dotenv =  require('dotenv')
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 new App(app);
 
-app.listen(process.env.PORT, () => {
+app
+  .listen(process.env.PORT, () => {
     console.info(`Server running on: ${process.env.PORT}`);
   })
-  .on('error', (err: any) => {
-    if (err.code === 'EADDRINUSE') {
-      console.log('server startup error: address already in use');
+  .on("error", (err: any) => {
+    if (err.code === "EADDRINUSE") {
+      console.log("server startup error: address already in use");
     } else {
       console.log(err);
     }
