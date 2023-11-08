aa0a366af6bb0d1ffa6973b9960d6733ec275b03
Franco Androetto <androettofranco@gmail.com>
2023-11-08 19:05:02 -0300

    build: test ver

--- app/routes/routes.ts
+++ app/routes/routes.ts
@@ -1,9 +1,10 @@
-import { Router } from 'express';
+import { Response, Router } from 'express';
 import authMiddleware from '../middleware/auth.middleware';
 import { getPrintArticle } from '../controllers/get-print-article.controller';
 import { getPrintTransaction } from '../controllers/get-print-transaction';
 import { getImage } from '../controllers/get-image.controller';
 import { getPrintArticles } from '../controllers/get-print-articles.controller';
+import RequestWithUser from 'interfaces/requestWithUser.interface';
 
 class PrintRoutes {
   router = Router();
@@ -17,6 +18,12 @@ class PrintRoutes {
     this.router.get('/transaction', [authMiddleware], getPrintTransaction);
     this.router.get('/get-img', [authMiddleware], getImage);
     this.router.post('/get-articles',[authMiddleware], getPrintArticles)
+
+    this.router.get('/ver', (  
+      req: RequestWithUser,
+      res: Response) => {    
+      res.status(200).send({ ver: '1' });
+    });
   }
 }
 export default new PrintRoutes().router;
